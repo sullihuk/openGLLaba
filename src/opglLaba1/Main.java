f65c704dd8fb9a533b724b2c8d26371a7467dbb2
sullihuk <nyxa@nextmail.ru>
2023-12-14 05:13:36 -0500

    First commit

---
+++ src/opglLaba1/Main.java
@@ -0,0 +1,14 @@
+package opglLaba1;
+
+public class Main {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		Game game = new Game();
+	game.play();
+
+	}
+
+}
+
+
