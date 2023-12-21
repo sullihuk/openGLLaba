eb441f3839c6954c59bcf48b554cc77b3c6a5074
sullihuk <nyxa@nextmail.ru>
2023-12-21 07:06:24 -0500

    Third commmit

--- src/opglLaba1/Game.java
+++ src/opglLaba1/Game.java
@@ -1,35 +1,57 @@
 package opglLaba1;
-import com.jogamp.opengl.GL4;
-import com.jogamp.opengl.GLCapabilities;
-import com.jogamp.opengl.GLAutoDrawable;
- import com.jogamp.opengl.GLEventListener;
- import com.jogamp.opengl.GLProfile;
- import com.jogamp.opengl.awt.GLCanvas;
- import javax.swing.JFrame;
+import java.nio.*;
+import javax.swing.*;
+import java.lang.Math;
+import com.jogamp.common.nio.Buffers;
+import static com.jogamp.opengl.GL4.*;
+import com.jogamp.opengl.*;
+import com.jogamp.opengl.awt.GLCanvas;
+import com.jogamp.opengl.math.Matrix4f;
+import com.jogamp.common.nio.Buffers;
+import com.jogamp.opengl.GLContext;
+import org.joml.*;
+
  
 public class Game extends JFrame implements GLEventListener{
-	private static final long serialVersionUID = 1L;
+	//private static final long serialVersionUID = 1L;
+	
+	private GLCanvas myCanvas;
+	
+	private int renderingProgram;
+	private int vao[] = new int[1];
+	private int vbo[] = new int[2];
+	private float cameraX,cameraY,cameraZ;
+	private float cubeLocX,cubeLocY,cubeLocZ;
+	
+	private FloatBuffer vals = Buffers.newDirectFloatBuffer(16); 
+	private Matrix4f pMat = new Matrix4f(); 
+	private Matrix4f vMat = new Matrix4f(); 
+	private Matrix4f mMat = new Matrix4f(); 
+	private Matrix4f mvMat = new Matrix4f(); 
+	private int mvLoc, pLoc; 
+	private float aspect; 
+	
 	
 	final private int width = 800;
 	final private int height = 600;
 
 	public Game () {
-		super("Minimal OpenGl");
-		GLProfile profile = GLProfile.get(GLProfile.GL4);
-		GLCapabilities capabilities = new GLCapabilities(profile);
+		setTitle("CUBE");
+		setSize(width, height);
+		//GLProfile profile = GLProfile.get(GLProfile.GL4);
+		//GLCapabilities capabilities = new GLCapabilities(profile);
 		
-		GLCanvas canvas = new GLCanvas(capabilities);
+		GLCanvas canvas = new GLCanvas();
 		canvas.addGLEventListener(this);
 		
-		this.setName("Laba OpenGL");
-		this.getContentPane().add(canvas);
+		//this.setName("Laba OpenGL");
+		//this.getContentPane().add(canvas);
 		
-		this.setSize(width, height);
-		this.setLocationRelativeTo(null);
+		//this.setLocationRelativeTo(null);
 		this.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
 		this.setVisible(true);
-		this.setResizable(false);
-		canvas.requestFocusInWindow();
+		this.setResizable(true);
+		//canvas.requestFocusInWindow();
 	}
 	
 
@@ -51,7 +73,9 @@ public class Game extends JFrame implements GLEventListener{
 
 	@Override
 	public void init (GLAutoDrawable drawable) {
-		GL4 gl = drawable.getGL().getGL4();
+		GL4 gl = (GL4)GLContext.getCurrentGL();
+		renderingProgram = Utils.createShaderProgram("vertShader.glsl", "fragShader.glsl");
+		setupVertices();
 		gl.glClearColor(0.392f, 0.584f, 0929f, 1.0f);
 	}
 
