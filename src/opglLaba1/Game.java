a3a0110877f90db33703ff0e9c2c842bd39eb292
sullihuk <nyxa@nextmail.ru>
2023-12-26 06:22:21 -0500

    Forth commit

--- src/opglLaba1/Game.java
+++ src/opglLaba1/Game.java
@@ -1,86 +1,108 @@
 package opglLaba1;
-import java.nio.*;
-import javax.swing.*;
-import java.lang.Math;
-import com.jogamp.common.nio.Buffers;
-import static com.jogamp.opengl.GL4.*;
-import com.jogamp.opengl.*;
-import com.jogamp.opengl.awt.GLCanvas;
-import com.jogamp.opengl.math.Matrix4f;
-import com.jogamp.common.nio.Buffers;
-import com.jogamp.opengl.GLContext;
-import org.joml.*;
+import java.awt.DisplayMode;
+import javax.swing.JFrame;
 
+import com.jogamp.opengl.GL2;
+import com.jogamp.opengl.GLAutoDrawable;
+import com.jogamp.opengl.GLEventListener;
+import com.jogamp.opengl.glu.GLU;
+import com.jogamp.opengl.util.FPSAnimator;
+
+public class Game implements GLEventListener {
+
+   public static DisplayMode dm, dm_old;
+   private GLU glu = new GLU();
+   private float rquad = 0.0f;
+      
+   @Override
+   public void display( GLAutoDrawable drawable ) {
+	
+      final GL2 gl = drawable.getGL().getGL2();
+      gl.glClear(GL2.GL_COLOR_BUFFER_BIT | GL2.GL_DEPTH_BUFFER_BIT );
+      gl.glLoadIdentity();
+      gl.glTranslatef( 0f, 0f, -5.0f ); 
+
+      // Rotate The Cube On X, Y & Z
+      gl.glRotatef(rquad, 1.0f, 1.0f, 1.0f); 
  
-public class Game extends JFrame implements GLEventListener{
-	//private static final long serialVersionUID = 1L;
-	
-	private GLCanvas myCanvas;
-	
-	private int renderingProgram;
-	private int vao[] = new int[1];
-	private int vbo[] = new int[2];
-	private float cameraX,cameraY,cameraZ;
-	private float cubeLocX,cubeLocY,cubeLocZ;
-	
-	private FloatBuffer vals = Buffers.newDirectFloatBuffer(16); 
-	private Matrix4f pMat = new Matrix4f(); 
-	private Matrix4f vMat = new Matrix4f(); 
-	private Matrix4f mMat = new Matrix4f(); 
-	private Matrix4f mvMat = new Matrix4f(); 
-	private int mvLoc, pLoc; 
-	private float aspect; 
-	
-	
-	final private int width = 800;
-	final private int height = 600;
+      //giving different colors to different sides
+      gl.glBegin(GL2.GL_QUADS); // Start Drawing The Cube
+      gl.glColor3f(1f,0f,0f); //red color
+      gl.glVertex3f(1.0f, 1.0f, -1.0f); // Top Right Of The Quad (Top)
+      gl.glVertex3f( -1.0f, 1.0f, -1.0f); // Top Left Of The Quad (Top)
+      gl.glVertex3f( -1.0f, 1.0f, 1.0f ); // Bottom Left Of The Quad (Top)
+      gl.glVertex3f( 1.0f, 1.0f, 1.0f ); // Bottom Right Of The Quad (Top)
+		
+      gl.glColor3f( 0f,1f,0f ); //green color
+      gl.glVertex3f( 1.0f, -1.0f, 1.0f ); // Top Right Of The Quad
+      gl.glVertex3f( -1.0f, -1.0f, 1.0f ); // Top Left Of The Quad
+      gl.glVertex3f( -1.0f, -1.0f, -1.0f ); // Bottom Left Of The Quad
+      gl.glVertex3f( 1.0f, -1.0f, -1.0f ); // Bottom Right Of The Quad 
 
-	public Game () {
-		setTitle("CUBE");
-		setSize(width, height);
-		//GLProfile profile = GLProfile.get(GLProfile.GL4);
-		//GLCapabilities capabilities = new GLCapabilities(profile);
+      gl.glColor3f( 0f,0f,1f ); //blue color
+      gl.glVertex3f( 1.0f, 1.0f, 1.0f ); // Top Right Of The Quad (Front)
+      gl.glVertex3f( -1.0f, 1.0f, 1.0f ); // Top Left Of The Quad (Front)
+      gl.glVertex3f( -1.0f, -1.0f, 1.0f ); // Bottom Left Of The Quad
+      gl.glVertex3f( 1.0f, -1.0f, 1.0f ); // Bottom Right Of The Quad 
+
+      gl.glColor3f( 1f,1f,0f ); //yellow (red + green)
+      gl.glVertex3f( 1.0f, -1.0f, -1.0f ); // Bottom Left Of The Quad
+      gl.glVertex3f( -1.0f, -1.0f, -1.0f ); // Bottom Right Of The Quad
+      gl.glVertex3f( -1.0f, 1.0f, -1.0f ); // Top Right Of The Quad (Back)
+      gl.glVertex3f( 1.0f, 1.0f, -1.0f ); // Top Left Of The Quad (Back)
+
+      gl.glColor3f( 1f,0f,1f ); //purple (red + green)
+      gl.glVertex3f( -1.0f, 1.0f, 1.0f ); // Top Right Of The Quad (Left)
+      gl.glVertex3f( -1.0f, 1.0f, -1.0f ); // Top Left Of The Quad (Left)
+      gl.glVertex3f( -1.0f, -1.0f, -1.0f ); // Bottom Left Of The Quad
+      gl.glVertex3f( -1.0f, -1.0f, 1.0f ); // Bottom Right Of The Quad 
+
+      gl.glColor3f( 0f,1f, 1f ); //sky blue (blue +green)
+      gl.glVertex3f( 1.0f, 1.0f, -1.0f ); // Top Right Of The Quad (Right)
+      gl.glVertex3f( 1.0f, 1.0f, 1.0f ); // Top Left Of The Quad
+      gl.glVertex3f( 1.0f, -1.0f, 1.0f ); // Bottom Left Of The Quad
+      gl.glVertex3f( 1.0f, -1.0f, -1.0f ); // Bottom Right Of The Quad
+      gl.glEnd(); // Done Drawing The Quad
+      gl.glFlush();
 		
-		GLCanvas canvas = new GLCanvas();
-		canvas.addGLEventListener(this);
-		
-		//this.setName("Laba OpenGL");
-		//this.getContentPane().add(canvas);
-		
-		//this.setLocationRelativeTo(null);
-		this.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
-		this.setVisible(true);
-		this.setResizable(true);
-		//canvas.requestFocusInWindow();
-	}
+      rquad -= 0.15f;
+   }
+   
+   @Override
+   public void dispose( GLAutoDrawable drawable ) {
+      // TODO Auto-generated method stub
+   }
+   
+   @Override
+   public void init( GLAutoDrawable drawable ) {
 	
-
-	public void play () {
-	}
+      final GL2 gl = drawable.getGL().getGL2();
+      gl.glShadeModel( GL2.GL_SMOOTH );
+      gl.glClearColor( 0f, 0f, 0f, 0f );
+      gl.glClearDepth( 1.0f );
+      gl.glEnable( GL2.GL_DEPTH_TEST );
+      gl.glDepthFunc( GL2.GL_LEQUAL );
+      gl.glHint( GL2.GL_PERSPECTIVE_CORRECTION_HINT, GL2.GL_NICEST );
+   }
+      
+   @Override
+   public void reshape( GLAutoDrawable drawable, int x, int y, int width, int height ) {
 	
-	@Override
-	public void display (GLAutoDrawable drawable) {
+      // TODO Auto-generated method stub
+      final GL2 gl = drawable.getGL().getGL2();
+      if( height == 0 )
+         height = 1;
+			
+      final float h = ( float ) width / ( float ) height;
+      gl.glViewport( 0, 0, width, height );
+      gl.glMatrixMode( GL2.GL_PROJECTION );
+      gl.glLoadIdentity();
 		
-		GL4 gl = drawable.getGL().getGL4();
-		gl.glClear(GL4.GL_COLOR_BUFFER_BIT |GL4.GL_DEPTH_BUFFER_BIT);
-		gl.glFlush();
-	}
-
-	@Override
-	public void dispose (GLAutoDrawable drawable) {
-		
-	}
-
-	@Override
-	public void init (GLAutoDrawable drawable) {
-		GL4 gl = (GL4)GLContext.getCurrentGL();
-		renderingProgram = Utils.createShaderProgram("vertShader.glsl", "fragShader.glsl");
-		setupVertices();
-		gl.glClearColor(0.392f, 0.584f, 0929f, 1.0f);
-	}
-
-	@Override
-	public void reshape (GLAutoDrawable drawable, int x, int y, int width, int height) {
-		
-	}
+      glu.gluPerspective( 45.0f, h, 1.0, 20.0 );
+      gl.glMatrixMode( GL2.GL_MODELVIEW );
+      gl.glLoadIdentity();
+   }
+      
+   
+	
 }
--- src/opglLaba1/Main.java
+++ src/opglLaba1/Main.java
@@ -1,14 +1,37 @@
 package opglLaba1;
 
+import javax.swing.JFrame;
+
+import com.jogamp.opengl.GLCapabilities;
+import com.jogamp.opengl.GLProfile;
+import com.jogamp.opengl.awt.GLCanvas;
+import com.jogamp.opengl.util.FPSAnimator;
+
 public class Main {
 
-	public static void main(String[] args) {
-		// TODO Auto-generated method stub
-		Game game = new Game();
-	game.play();
-
+	
+		public static void main( String[] args ) {
+			
+		      final GLProfile profile = GLProfile.get( GLProfile.GL2 );
+		      GLCapabilities capabilities = new GLCapabilities( profile );
+		      
+		      // The canvas
+		      final GLCanvas glcanvas = new GLCanvas( capabilities );
+		      Game cube = new Game();
+				
+		      glcanvas.addGLEventListener( cube );
+		      glcanvas.setSize( 400, 400 );
+				
+		      final JFrame frame = new JFrame ( " Multicolored cube" );
+		      frame.getContentPane().add( glcanvas );
+		      frame.setSize( frame.getContentPane().getPreferredSize() );
+		      frame.setVisible( true );
+		      final FPSAnimator animator = new FPSAnimator(glcanvas, 300,true);
+				
+		      animator.start();
+		   }
 	}
 
-}
+
 
 
